1a7ac69876ffdf039b94728d5b501a1e7e28532c
caskerin <60627176+caskerin@users.noreply.github.com>
2020-04-06 13:56:03 -0500

    favfoodsfixit10

--- forms/favFoods/favFoods.js
+++ forms/favFoods/favFoods.js
@@ -1,1 +1,22 @@
-let goodFoods = ["pizza","apples","cookies","sushi","pasta","steak"]
+
+let goodFood = ["pizza","apples","cookies","sushi","pasta","steak"]
+
+favFoods.onshow=function(){
+  lstFood.clear()
+  for (i=0; i <= goodFood.length -1; i++) {
+    lstFood.addItem(goodFood[i])
+}
+}
+
+
+lstFood.onclick=function(choice){
+  if (typeof(choice) == "object")
+  return
+  let foodChoice = goodFood[choice]
+  lblAnswer.value =`You picked ${foodChoice}`
+}
+
+btnDessert.onclick=function(){
+  ChangeForm(dessertVoting)
+}
+
